9967c37a431fce9ad7f631baef9bfb9af90a8051
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2022-11-13 20:33:48 -0600

    Agregando funcionamiento al carrito de compras con sessionStorage

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 API_URL = 'https://test-backend-bsale.herokuapp.com/api/v0/';
+// API_URL = 'http://localhost:8000/api/v0/';
 
 function showAlert(message) {
     var alerta = '<div class="alert alert-warning" role="alert">'+ message +'</div>';
@@ -9,9 +10,70 @@ function setTitleCategory(cat) {
     $("#title-category").html(cat);
 }
 
+function getListBuy(){
+    if (typeof(Storage) != undefined){
+        let cart_list = sessionStorage.getItem('cart_list');
+        let total = parseInt($("#products-list").html());
+        // Verificamos si encontramos item guardados para el carrito
+        if (cart_list != null){
+            var data = JSON.parse(cart_list);
+            $("#products-list").html(data.products.length);
+        }
+    }
+}
+
+/**
+ * Ejemplo sencillo de carrito de compras usando el sessionStorage
+ */
+function addItemListCart(id, name, price){
+    // Obtener listado de item en el carrito de compras
+    if (typeof(Storage) != undefined){
+        let cart_list = sessionStorage.getItem('cart_list');
+        let total = parseInt($("#products-list").html());
+        // Verificamos si encontramos item guardados para el carrito
+        if (cart_list == null){
+            var articles = JSON.stringify({'products':[{'id':id, 'name': name, 'price': price}]});
+            sessionStorage.setItem('cart_list', articles);
+            $("#products-list").html('1');
+        }else{
+            var data = JSON.parse(cart_list);
+            // Agregar el elemento a la lista si es que no se encuentra dentro de ella
+            var addElement = true;
+            // Verificar si el elemento existe dentro de la lista
+            for (let i = 0; i < data.products.length; i++) {
+                if (data.products[i].id == id){
+                    addElement = false;
+                }
+            }
+
+            if (addElement){
+                data.products[data.products.length] = {'id' : id, 'name': name, 'price': price};
+                // Guardar la lista en la sessionStorage
+                sessionStorage.setItem('cart_list', JSON.stringify(data));
+                $("#products-list").html(total + 1);
+            }
+        }
+    }
+}
+
+function setActionAddCart() {
+    $(".add-cart").on('click', function(e){
+        e.preventDefault();
+        addItemListCart(
+            $(this).data('id'),
+            $(this).data('name'),
+            $(this).data('price')
+        )
+    });
+}
+
 function getProductosByCategory(id_cat) {
+    // Limpiar contenido de la lista de productos
     $("#list-products").html('');
+    // Limpiar alerta
     $("#alerta-msg").html('');
+
+    // Obtener los productos por ajax relacionados con la categoria
     $.ajax({
         type: "get",
         url: API_URL + "categories/" + id_cat,
@@ -19,7 +81,7 @@ function getProductosByCategory(id_cat) {
         success: function (data) {
             if (data.message == 'success'){
                 
-                // Verificar si el producto tiene descuento
+                // Recorrer item por item para generar la tarjeta
                 for (let i = 0; i < data.products.length; i++) {
                     const p = data.products[i];
                     // Cargamos el template de la tarjeta
@@ -37,6 +99,8 @@ function getProductosByCategory(id_cat) {
                         $("#list-products").append(result);
                     }
                 }
+
+                setActionAddCart();
             }else{
                 showAlert(data.message)
             }
@@ -75,4 +139,6 @@ $(function(){
             });
         }
     });
+
+    getListBuy();
 });
